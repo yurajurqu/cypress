de76cf5544e2ab62855d3575c1089aacbc1b0058
obarboza <obarboza@pe.ibm.com>
2021-05-16 10:33:20 -0500

    update

--- cypress/integration/examples/ea.spec.js
+++ cypress/integration/examples/ea.spec.js
@@ -4,7 +4,22 @@ describe("Testing of EA app", () => {
     
     it("login application", () => {
         cy.visit("http://eaapp.somee.com/");
-        cy.contains("Login").click();
+
+        //long way
+        // cy.get("#loginLink").then(($link) => {
+        //     // const linkText = $link.text();
+        //     // expect(linkText).is.eql('Login');
+        //     return $link.text();
+        // }).as('linkText');
+
+        //short way with invoke
+        cy.get("#loginLink").invoke('text').as('linkText');
+
+        cy.get('@linkText').then(($x) => {
+            expect($x).is.eql('Login');
+        });
+
+        cy.get('#loginLink').click();
 
         cy.url().should("include", "/Account/Login");
 
@@ -15,7 +30,18 @@ describe("Testing of EA app", () => {
 
         cy.contains('Employee List').click();
 
-        cy.get('.table').find('tr > td').contains('Prashanth').parent().contains('Benefits').click(); 
+        // cy.get('.table').find('tr > td').contains('Prashanth').parent().contains('Benefits').click();
+        // cy.get('.table').find('tr').as('rows');
+        // cy.get('@rows').then(($row) => {
+        //     cy.wrap($row).click({multiple:true});
+        // });
+
+        //equivalent to previous block using wrap
+        cy.get('.table').find('tr > td').then(($td) => {
+            cy.wrap($td).contains('John').invoke('wrap').parent().contains('Benefits').click();
+        });
+
+        cy.wrap({ name: 'Karthik' }).should('have.property','name').and('eq','Karthik');
 
     });
     // it("visit portal compras", () => {
