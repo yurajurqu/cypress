f4b1f129f9acdd81a21f8d09b33ceda40ae84087
obarboza <obarboza@pe.ibm.com>
2021-05-21 09:58:37 -0500

    environment config

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -10,12 +10,27 @@
 
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
-
+const fs = require('fs-extra')
+const path= require('path')
 const cucumber = require('cypress-cucumber-preprocessor').default
-
 
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
-  on('file:preprocessor',cucumber())
+  on('file:preprocessor', cucumber())
+  
+
+  function processConfigName(on, config) {
+    const file = config.env.name  || 'default'
+    return getConfigFile(file).then(function (file) {
+        // file.baseUrl = file.baseUrl + process.env.URI_ROOT
+        return file;
+    })
+  }
+
+  function getConfigFile(file) {
+    const pathToConfigFile = path.resolve('cypress', 'config', `${file}.json`)
+    return fs.readJson(pathToConfigFile)
+  }
+  return processConfigName(on, config);
 }
